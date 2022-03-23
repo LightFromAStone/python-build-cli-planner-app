13cd4ef39c63f658907dc13fa6c3a1b559144c4e
LightFromAStone <github@lightfromastone.me>
2022-03-23 14:57:21 -0600

    completed through part 7

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, ABC, abstractmethod
 from collections.abc import Iterable
 from dateutil.parser import parse
+from datetime import datetime
 
 class DeadlinedMetaReminder(Iterable, metaclass= ABCMeta):
     @abstractmethod
@@ -18,3 +19,9 @@ class DateReminder(DeadlinedReminder):
     def __init__(self, text, date):
         self.date = parse(date, dayfirst=True)
         self.text = text
+        
+    def is_due(self):
+        return self.date <= datetime.now()
+    
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])
